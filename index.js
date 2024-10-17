0e9ea5f131859f79a98b42599e0a87928007bb6a
malu-sree <sreelakshmipalangattu@gmail.com>
2024-10-16 23:04:49 -0700

    second commit

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const express=require('express')
-const cors=require('cors');
-const { request } = require('http');
+const express = require('express');
+const cors = require('cors');
 
-const app=express();
-app.use(cors())
-//http:localhost:9000/home->api
-app.get("/home",(req,res)=>{
-    let array=["Apple","Orange","mango"]
-    res.send(array)
-})
-app.get("/",(req,res)=>{
-    res.send('home page')
-})
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sample data for /home route
+app.get("/home", (req, res) => {
+    let array = [
+        { fruitName: 'Apple', rate: '145', quality: 'A' },
+        { fruitName: 'Orange', rate: '45', quality: 'B' },
+        { fruitName: 'Banana', rate: '100', quality: 'C' },
+        { fruitName: 'Grapes', rate: '145', quality: 'A' },
+    ];
+    res.send(array);
+});
 
 
+app.post("/add", (req, res) => {
+    const { num1, num2 } = req.body; 
+    const sum = num1 + num2; 
 
-app.listen(9000,()=>{
-    console.log('server running http://localhost:9000')
-})
+    
+    console.log(`Received numbers: ${num1}, ${num2}. Sum: ${sum}`);
 
-//npm init -y
-//npm i express nodemon cors
+   
+    res.send({ sum });   
+});
+
+
+app.post("/userpost", (req, res) => { 
+    const { username, email, mobile, message } = req.body; 
+    console.log(req.body);
+    res.send("Successfully posted user data.");
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+
+app.listen(9000, () => {
+    console.log("Server running at http://localhost:9000");
+});
